36febfb5d9ca7f7f9884d39c849311e4bee141af
meenaranga <117272695+meenaranga@users.noreply.github.com>
2023-03-19 00:08:30 -0700

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,16 @@
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBlnupslLcGijvT1Fb5xu-BStWLvRSe_Wk",
+    authDomain: "class-101-6f557.firebaseapp.com",
+    databaseURL: "https://class-101-6f557-default-rtdb.firebaseio.com",
+    projectId: "class-101-6f557",
+    storageBucket: "class-101-6f557.appspot.com",
+    messagingSenderId: "322331760979",
+    appId: "1:322331760979:web:b3c59fcb67a8816a6de9a6",
+    measurementId: "G-DJL30VXFG8"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
